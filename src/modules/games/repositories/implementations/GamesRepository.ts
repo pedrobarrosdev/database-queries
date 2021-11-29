4943d227d1357dbe0870b04d3c5398e6c09b6303
Pedro Campos <pedrogcamposb@outlook.com>
2021-11-29 20:02:17 -0300

    Initial commit

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,30 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    // Complete usando query builder
+    return this.repository
+      .createQueryBuilder("game").where(`game.title ILIKE '%${param}%'`).getMany()
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    // Complete usando raw query
+    return this.repository.query("SELECT COUNT(*) FROM games");
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    // Complete usando query builder
+    return this.repository.createQueryBuilder().relation("users").of(id).loadMany();
+  }
+}
